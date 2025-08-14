a33257088ed60d246a981659c4d9d9e9201ba505
Vahe <vahe.simonyan@polymorphic.io>
2025-08-14 18:42:50 +0400

    new file

---
+++ cypress/e2e/SDS/Error_fixing.cy.js
@@ -0,0 +1,10 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+});
+
+describe('SDS-LendWyse Automation', () => {
+    // beforeEach(() => {
+    //     cy.viewport(390,844);
+    // });
+
+});
