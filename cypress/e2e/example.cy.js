09ef441b396beb3b9f1cb232a2ddf20bf581b969
Vahe <vahe.simonyan@polymorphic.io>
2026-05-21 11:34:38 +0400

    delete

--- cypress/e2e/example.cy.js
+++
@@ -1,7 +0,0 @@
-describe('My First Test', () => {
-    it('Visits the Cypress Documentation Page', () => {
-      cy.visit('https://docs.cypress.io') // Navigates to the page
-      cy.contains('Introduction').should('be.visible') // Checks for text visibility
-    })
-  })
-  
